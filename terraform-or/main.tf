278b26d587781389f9aa9c5d6203bace5cb62fcf
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2024-06-15 20:24:04 -0400

    annoying trees

--- terraform-or/main.tf
+++ terraform-or/main.tf
@@ -14,6 +14,7 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
 ############################
 ### Resources with Tags ### 
 ###########################
@@ -21,6 +22,13 @@ resource "aws_instance" "or_server" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+              
+  ### Name tags added ###
   tags = {
     Name = "new_h_server"
   }
